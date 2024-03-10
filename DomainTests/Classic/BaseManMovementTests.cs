9c42e5659cd9cc61dc159e66e89c0f8a0cfacd7d
Adam Praszywka <praszywka.adam@gmail.com>
2024-03-10 22:01:21 +0100

    Unit tests for White Piece moves

--- DomainTests/Classic/BaseManMovementTests.cs
+++ DomainTests/Classic/BaseManMovementTests.cs
@@ -23,20 +23,20 @@ public class BaseManMovementTests
         Assert.That(board.Snapshot.ToTestSquares()[targetPosition.Row, targetPosition.Column], Is.EqualTo(TestSquare.WhiteMan));
     }
     
-    [Test]
-    [TestCaseSource(typeof(WhiteManForbiddenMovesTestData))]
-    public void WhiteForbiddenMove(Position initialPosition, Position targetPosition)
-    {
-        var pieces = new List<(Piece, Position)> {(new Man("MyTestPiece", Color.White), initialPosition)};
-
-        var configuration = ClassicConfiguration.FromSnapshot(pieces);
-        var board = new Board(configuration);
-
-        var result = board.Move("MyTestPiece", targetPosition);
-
-        Assert.That(result.IsSuccess);
-        Assert.That(board.Snapshot.ToTestSquares()[targetPosition.Row, targetPosition.Column], Is.EqualTo(TestSquare.WhiteMan));
-    }
+    // [Test]
+    // [TestCaseSource(typeof(WhiteManForbiddenMovesTestData))]
+    // public void WhiteForbiddenMove(Position initialPosition, Position targetPosition)
+    // {
+    //     var pieces = new List<(Piece, Position)> {(new Man("MyTestPiece", Color.White), initialPosition)};
+    //
+    //     var configuration = ClassicConfiguration.FromSnapshot(pieces);
+    //     var board = new Board(configuration);
+    //
+    //     var result = board.Move("MyTestPiece", targetPosition);
+    //
+    //     Assert.That(result.IsSuccess);
+    //     Assert.That(board.Snapshot.ToTestSquares()[targetPosition.Row, targetPosition.Column], Is.EqualTo(TestSquare.WhiteMan));
+    // }
     
     // [Test]
     // [TestCaseSource(typeof(BlackManMoveForwardTestData))]
