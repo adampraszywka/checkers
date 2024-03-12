b191a034dc13ef5c6073f43e1151b635ec30d0da
Adam Praszywka <praszywka.adam@gmail.com>
2024-03-12 22:34:46 +0100

    Merge Piece & ObservablePiece into Piece Remove connection from Piece to Square

--- Domain/Pieces/King.cs
+++ Domain/Pieces/King.cs
@@ -7,19 +7,4 @@ public class King(string id, Color color) : Piece
     public string Id => id;
     public Color Color => color;
     public Type Type => Type.King; 
-    public Square? Square { get; private set; }
-    public void Attach(Square square)
-    {
-        Square = square;
-    }
-
-    public void Remove()
-    {
-        Square = null;
-    }
-
-    public IEnumerable<Move> PossibleMoves(BoardSnapshot snapshot)
-    {
-        throw new NotImplementedException();
-    }
 }
