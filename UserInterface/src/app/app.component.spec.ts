686baa654367c91d6152450feceb83c73fae2cf7
Adam Praszywka <praszywka.adam@gmail.com>
2024-03-15 22:29:09 +0100

    Remove default Angular component

--- UserInterface/src/app/app.component.spec.ts
+++ UserInterface/src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('UserInterface');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UserInterface');
-  });
 });
--- UserInterface/src/app/app.component.ts
+++ UserInterface/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BoardComponent} from "./board/board/board.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
