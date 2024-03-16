29615c4e7a10347311a6aa4058b71e85613699c6
Adam Praszywka <praszywka.adam@gmail.com>
2024-03-16 18:45:46 +0100

    PoC WebAPI endpoint

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,29 @@
+using WebApi.Dto;
+using WebApi.Repository;
+
+const string devCors = "_devCors";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<BoardRepository, InMemoryBoardRepository>();
+
+// For PoC development. Needs to be reworked later
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy(devCors, p => p.WithOrigins("http://localhost:4200"));
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/board", async (BoardRepository repository) =>
+{
+    var board = await repository.Get();
+    var snapshot = board.Snapshot;
+    return new BoardDto(snapshot);
+});
+
+app.UseCors(devCors);
 
 app.Run();
---
+++ WebApi/Repository/InMemoryBoardRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using Domain.Configurations.Classic;
+
+namespace WebApi.Repository;
+
+public class InMemoryBoardRepository : BoardRepository
+{
+    private readonly Board _board;
+    
+    public InMemoryBoardRepository()
+    {
+        var config = ClassicConfiguration.NewBoard();
+        _board = new Board(config);
+    }
+    
+    public Task<Board> Get()
+    {
+        return Task.FromResult(_board);
+    }
+}
