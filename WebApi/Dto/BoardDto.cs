770189844c6dff3778c1425ad96087a044cf5abc
Adam Praszywka <praszywka.adam@gmail.com>
2024-03-17 18:47:44 +0100

    Return Squares as list of lists

--- WebApi/Dto/BoardDto.cs
+++ WebApi/Dto/BoardDto.cs
@@ -3,16 +3,16 @@ using Extension;
 
 namespace WebApi.Dto;
 
-public class BoardDto
+public record BoardDto
 {
     public int Columns { get; }
     public int Rows { get; }
-    public IEnumerable<SquareSnapshot> Squares { get; }
+    public IEnumerable<IEnumerable<SquareSnapshot>> Squares { get; }
     
     public BoardDto(BoardSnapshot snapshot)
     {
         Columns = snapshot.Columns;
         Rows = snapshot.Rows;
-        Squares = snapshot.Squares.ReverseRows().Flatten();
+        Squares = snapshot.Squares.ReversedRowsListOfLists();
     }
 }
