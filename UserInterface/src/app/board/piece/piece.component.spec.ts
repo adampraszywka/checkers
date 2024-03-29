68671eba9ea58522618ff4c792cb8af2864a63ed
Adam Praszywka <praszywka.adam@gmail.com>
2024-03-29 22:32:33 +0100

    UI - differentiate piece and king on the board

--- UserInterface/src/app/board/piece/piece.component.spec.ts
+++ UserInterface/src/app/board/piece/piece.component.spec.ts
@@ -2,6 +2,7 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PieceComponent} from './piece.component';
 import {Color, Type} from "../dto/piece.interface";
+import {By} from "@angular/platform-browser";
 
 describe('PieceComponent', () => {
   let component: PieceComponent;
@@ -24,4 +25,36 @@ describe('PieceComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('white man', () => {
+    component.piece = {id: '', color: Color.White, type: Type.Man};
+    fixture.detectChanges();
+    const classes = fixture.debugElement.query(By.css('div')).classes;
+
+    expect(classes).toEqual({white: true, man: true})
+  })
+
+  it('white king', () => {
+    component.piece = {id: '', color: Color.White, type: Type.King};
+    fixture.detectChanges();
+    const classes = fixture.debugElement.query(By.css('div')).classes;
+
+    expect(classes).toEqual({white: true, king: true})
+  })
+
+  it('black man', () => {
+    component.piece = {id: '', color: Color.Black, type: Type.Man};
+    fixture.detectChanges();
+    const classes = fixture.debugElement.query(By.css('div')).classes;
+
+    expect(classes).toEqual({black: true, man: true})
+  })
+
+  it('black king', () => {
+    component.piece = {id: '', color: Color.Black, type: Type.King};
+    fixture.detectChanges();
+    const classes = fixture.debugElement.query(By.css('div')).classes;
+
+    expect(classes).toEqual({black: true, king: true})
+  })
 });
--- UserInterface/src/app/board/piece/piece.component.ts
+++ UserInterface/src/app/board/piece/piece.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {Color, Piece} from "../dto/piece.interface";
+import {Color, Piece, Type} from "../dto/piece.interface";
 import {NgClass} from "@angular/common";
 
 @Component({
@@ -14,4 +14,14 @@ import {NgClass} from "@angular/common";
 export class PieceComponent {
   @Input() piece!: Piece;
   protected readonly Color = Color;
+
+  public get cssClasses(): string {
+    const color = this.piece.color === Color.Black ? 'black' : 'white';
+
+    if (this.piece.type === Type.Man) {
+      return `${color} man`
+    }
+
+    return `${color} king`
+  }
 }
