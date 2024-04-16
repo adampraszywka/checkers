b11abfdd949e3a71cf939ca50e05608ff9e88be2
Adam Praszywka <praszywka.adam@gmail.com>
2024-04-17 00:37:51 +0200

    Notify SignalR players on board update

---
+++ WebApi/Extensions/PieceExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Chessboard.Pieces;
+using WebApi.Dto.Response;
+
+namespace WebApi.Extensions;
+
+public static class PieceExtensions
+{
+    public static PieceDto ToDto(this Piece p) => new() {Id = p.Id, Color = p.Color, Type = p.Type};
+}
