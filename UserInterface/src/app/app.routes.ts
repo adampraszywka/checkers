aa3a8d88e03b3af2099d28487c392c7f5f80360c
Adam Praszywka <praszywka.adam@gmail.com>
2024-04-17 21:28:11 +0200

    Add Warp-Up page

--- UserInterface/src/app/app.routes.ts
+++ UserInterface/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '', pathMatch: 'full', loadComponent: () => import('./slow-webapp/slow-webapp.component').then(mod => mod.SlowWebappComponent)},
   {path: 'dashboard', loadComponent: () => import('./dashboard/dashboard/dashboard.component').then(mod => mod.DashboardComponent)},
   {path: 'lobby/:lobbyId', loadComponent: () => import('./lobby/lobby/lobby.component').then(mod => mod.LobbyComponent)},
   {path: 'board/:boardId', loadComponent: () => import('./board/board/board.component').then(mod => mod.BoardComponent)}
---
+++ UserInterface/src/app/slow-webapp/api-client.service.ts
@@ -0,0 +1,14 @@
+﻿import {Injectable} from "@angular/core";
+import {ApiConfiguration} from "../app.config";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Ping} from "./ping.interface";
+
+@Injectable()
+export class ApiClient {
+  constructor(private readonly config: ApiConfiguration, private readonly client: HttpClient) {}
+
+  public ping(): Observable<Ping> {
+    return this.client.get<Ping>(this.config.baseUrl + 'ping')
+  }
+}
