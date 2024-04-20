62b9065f72e34387a9c1f9f621f919de644dc175
Adam Praszywka <praszywka.adam@gmail.com>
2024-04-21 00:15:23 +0200

    Enforce each player to move piece with the highest capture count

---
+++ Domain/Chessboard/Errors/UnderperformingCaptureError.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+
+namespace Domain.Chessboard.Errors;
+
+public class UnderperformingCaptureError(Position sourceSelected, IEnumerable<Position> sourceAvailable) : Error(BuildMessage(sourceSelected, sourceAvailable))
+{
+    private static string BuildMessage(Position sourceSelected, IEnumerable<Position> sourceAvailable) =>
+        $"The piece at {sourceSelected.Name} is unable to move. Pieces at {string.Join(", ", sourceAvailable.Select(x => x.Name))} have the capability to capture multiple opponent pieces, potentially more than the piece at {sourceSelected.Name}.";
+}
