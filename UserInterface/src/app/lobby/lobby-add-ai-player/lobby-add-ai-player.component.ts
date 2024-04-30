a8e2527717dc849de9ba5cefa9210c2293bec46a
Adam Praszywka <praszywka.adam@gmail.com>
2024-04-30 22:59:49 +0200

    Select first value in Create AI Player modal on initialization

--- UserInterface/src/app/lobby/lobby-add-ai-player/lobby-add-ai-player.component.ts
+++ UserInterface/src/app/lobby/lobby-add-ai-player/lobby-add-ai-player.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {AiPlayer} from "../../shared/dto/ai-player.interface";
@@ -13,16 +13,20 @@ import {ModalResult} from "../../shared/result/modal-result";
   templateUrl: './lobby-add-ai-player.component.html',
   styleUrl: './lobby-add-ai-player.component.scss'
 })
-export class LobbyAddAiPlayerComponent {
+export class LobbyAddAiPlayerComponent implements OnInit {
   @Input() aiPlayers: AiPlayer[] = [];
 
   public type: FormControl<string>;
   constructor(public readonly modal: NgbActiveModal) {
-    this.type = new FormControl<string>('', {nonNullable: true});
+    this.type = new FormControl<string>(this.aiPlayers[0]?.type, {nonNullable: true});
+
+  }
+
+  ngOnInit(): void {
+    this.type.setValue(this.aiPlayers[0]?.type);
   }
 
   onCreate(): void {
-
     this.modal.close(new ModalResult(this.type.value));
   }
 
