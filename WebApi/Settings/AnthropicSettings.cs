d3f95af2a215a682c9328fd421d0495cc764af6c
Adam Praszywka <praszywka.adam@gmail.com>
2024-05-08 22:51:35 +0200

    Configuration cleanup

---
+++ WebApi/Settings/AnthropicSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Settings;
+
+public record AnthropicSettings
+{
+    public const string Key = "Antropic";
+    
+    [Required(AllowEmptyStrings = false)]
+    public required string ApiKey { get; init; }
+}
---
+++ WebApi/Settings/OpenAISettings.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Settings;
+
+public record OpenAISettings
+{
+    public const string Key = "OpenAi";
+
+    [Required(AllowEmptyStrings = false)]
+    public required string ApiKey { get; init; }
+}
