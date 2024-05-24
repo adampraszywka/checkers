4ce6073ccf4c826ecd3622445fd3b18afd0ba5c8
Adam Praszywka <praszywka.adam@gmail.com>
2024-05-24 22:08:52 +0200

    Support invalid values in OpenAiGpt4oConfiguration

--- AIPlayers/Algorithms/OpenAIGpt4o/OpenAiGpt4oConfiguration.cs
+++ AIPlayers/Algorithms/OpenAIGpt4o/OpenAiGpt4oConfiguration.cs
@@ -13,7 +13,9 @@ public record OpenAiGpt4oConfiguration
     private const string RefereeField = "refereeEnabled";
     
     private const float DefaultTemperature = 0.2f;
-    private const string DefaultModel = "gpt-4o";
+    
+    private const string ModelGpt4Turbo = "gpt-4-turbo";
+    private const string ModelGpt4o = "gpt-4o";
     
     public OpenAiGpt4oConfiguration(Dictionary<string, string> genericConfiguration)
     {
@@ -25,8 +27,14 @@ public record OpenAiGpt4oConfiguration
         {
             Temperature = DefaultTemperature;
         }
-        
-        Model = genericConfiguration.GetValueOrDefault(ModelField, DefaultModel);
+
+        Model = ParseModel(genericConfiguration.GetValueOrDefault(ModelField));
         RefereeEnabled = genericConfiguration.TryGetValue(RefereeField, out var refereeEnabled) && bool.TryParse(refereeEnabled, out var referee) && referee;
+    }
+
+    private string ParseModel(string? value)
+    {
+        var trimmedValue = value?.Trim();
+        return trimmedValue is ModelGpt4Turbo or ModelGpt4o ? trimmedValue : ModelGpt4o;
     }
 }
